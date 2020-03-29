d5e6fe9d1b1050b775238edd792016f8e3456d89
Joshua Kim <jykim92@gmail.com>
2020-03-29 22:25:19 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -32,9 +32,6 @@ def valid_move?(board, index)
   end
 end
 
-until input_to_index == .between?(0, 8)
-  puts "Error, please submit a valid number"
-
 
 def position_taken?(board, index)
   if board[index] == " " || board[index] == "" || board[index] == nil
